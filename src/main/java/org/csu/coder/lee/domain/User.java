df4667f14f0980cacc3f80a6e6f2430f657f44bc
lixiang <csucoderlee@gmail.com>
2017-04-04 16:38:31 +0800

    add 数据库配置

--- src/main/java/org/csu/coder/lee/domain/User.java
+++ src/main/java/org/csu/coder/lee/domain/User.java
@@ -1,12 +1,30 @@
 package org.csu.coder.lee.domain;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
 /**
  * Created by csucoderlee on 2017 04 03 下午10:16.
  */
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String username;
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
